dd2f790fbb395471514786af76e966b5f90095fd
Muhammad Ahmad <cahmad045@gmail.com>
2024-09-12 23:53:24 +0500

    final commit

--- links/forms.py
+++ links/forms.py
@@ -4,3 +4,10 @@ class LinkForm(forms.ModelForm):
     class Meta:
         model = Links
         fields = ['name', 'url', 'slug']
+        labels = {
+            'name': 'Link Name',
+        }
+
+        widgets = {
+            'url': forms.TextInput(attrs={'placeholder': 'Enter URL here'})
+        }
